b11f1d14fa495beb22e33e480d3a29e9b46067ad
abhishek-pb1 <studentabhishekpb@gmail.com>
2023-08-07 15:07:33 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var inputBox = document.querySelector(".text-box #userInput");
+var userInput ='';
+var equalsButton = document.querySelector(".numpad .equals");
+
+// Keyboard presses
+document.addEventListener("keydown", function(event) {
+    switch (event.key) {
+        case "Enter":
+            calc();
+            break;
+        case "x":
+            inputBox.value += "*";
+            break;
+    
+    
+        default:
+            break;
+    }
+})
+
+// Mouse clicks
+
+// document.addEventListener("click", function(key) {
+//     switch (key) {
+//         case "Enter":
+//             calc();
+//             break;
+//         case "x":
+//             inputBox.value += '*';
+//             break;
+    
+    
+//         default:
+//             break;
+//     }
+// })
+
+
+
+equalsButton.addEventListener("click", function() { 
+    userInput = inputBox.value;
+    const result = '\n'+ eval(userInput);
+    inputBox.value += result;
+});
+
+function calc() { 
+    userInput = inputBox.value;
+    const result = '\n'+ eval(userInput);
+    inputBox.value += result;
+}
